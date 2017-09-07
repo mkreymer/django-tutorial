f4c2ed4f43014523a8eafdc18a23064fb15507de
mkreymer <michaelkreymer@propertypanorama.com>
2017-09-07 13:42:23 -0400

    added views

--- project/mysite/polls/views.py
+++ project/mysite/polls/views.py
@@ -6,3 +6,21 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+
+
+#these views are slightly different because they take an argument:
+
+def detail(request, question_id):
+	return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+	response = "You're looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s." % question_id)
+#the above must then be wired into the polls.urls module by adding url() calls
+
+
